518cea10b8214f5053e5439e18e56e612a372756
zhangwei <493514325@qq.com>
2020-04-02 10:01:35 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div class="header">header</div>
+    <router-view/>
+    <div class="footer">footer</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style lang="scss">
+  @import 'assets/styles/app.scss';
+</style>
---
+++ src/pages/HomePage/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home-page">
+    <div>{{ Msg }}</div>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from '@/components/HelloWorld'
+
+  export default {
+    name: 'HomePage',
+    data() {
+      return {
+        Msg: 'this is homepage'
+      }
+    },
+    components: {
+      HelloWorld
+    }
+  }
+</script>
