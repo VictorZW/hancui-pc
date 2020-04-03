df56ecf0b2396d9efca22b1e1569cf887f09a463
zhangwei <493514325@qq.com>
2020-04-03 18:14:15 +0800

    产品展示

--- src/components/PageHeader/index.vue
+++ src/components/PageHeader/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="page-header">
-    <div class="header-inner"></div>
+    <div class="header-inner">
+      <div class="left">
+        <span class="menu-btn menu-btn-active">首页</span>
+        <span class="menu-btn">产品展示</span>
+        <span class="menu-btn">芳疗学院</span>
+      </div>
+      <div class="logo"></div>
+      <div class="right">
+        <span class="menu-btn">企业资讯</span>
+        <span class="menu-btn">关于我们</span>
+        <span class="menu-btn">联系我们</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,6 +27,7 @@
     width: 100%;
     height: 59px;
     background-color: #069163;
+    opacity: 0.8;
     position: fixed;
     top: 0;
     left: 0;
@@ -22,8 +35,43 @@
     z-index: 100;
     .header-inner {
       width: 1200px;
+      min-width: 1200px;
       height: 100%;
       margin: auto;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      .logo {
+        width: 44px;
+        height: 44px;
+        background-size: cover;
+        background-image: url("~@IMG/LOGO.png");
+        opacity: 1;
+        margin-left: 90px;
+        margin-right: 90px;
+      }
+      .left, .right {
+        flex: 1;
+      }
+      .left {
+        text-align: right;
+      }
+      .right {
+        text-align: left;
+      }
+      .menu-btn {
+        display: inline-block;
+        font-size: 11px;
+        color: #FFFFFF;
+        padding: 4px 12px;
+        margin: 0 11px;
+        cursor: pointer;
+      }
+      .menu-btn-active {
+        color: #004033;
+        background-color: #FFFFFF;
+        border-radius: 3px;
+      }
     }
   }
 </style>
--- src/pages/HomePage/index.vue
+++ src/pages/HomePage/index.vue
@@ -1,10 +1,61 @@
 <template>
   <div id="pureFullPage" class="home-page">
-    <div class="page">
-      <p>Page 1</p>
-    </div>
-    <div class="page">
-      <p>Page 2</p>
+    <div class="page page-first"></div>
+    <div class="page page-second">
+      <div class="product-banner">
+        <div class="cover-model">
+          <!--产品文字说明-->
+          <div class="pro-msg">
+            <div class="pro-icon-area">
+              <div class="pro-icon pro-icon1"></div>
+            </div>
+            <div class="title">护肤品</div>
+            <div class="title">SKIN CARE</div>
+            <div class="detail">
+              汉萃主营产品是天然植物健康产品、芳香疗法和护肤肽类产品；研发6大系列40余种无添加任何化学成分的复方功效精油，已申请精油相关发明专利9项、肽相关发明专利7。每款产品针对不同肌肤问题提供专业解决方案。产品专注于安全健康，源自天然。
+            </div>
+            <div class="read-more">
+              <span>查看更多</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="product-banner">
+        <div class="cover-model">
+          <!--产品文字说明-->
+          <div class="pro-msg">
+            <div class="pro-icon-area">
+              <div class="pro-icon pro-icon1"></div>
+            </div>
+            <div class="title">护肤品</div>
+            <div class="title">SKIN CARE</div>
+            <div class="detail">
+              汉萃主营产品是天然植物健康产品、芳香疗法和护肤肽类产品；研发6大系列40余种无添加任何化学成分的复方功效精油，已申请精油相关发明专利9项、肽相关发明专利7。每款产品针对不同肌肤问题提供专业解决方案。产品专注于安全健康，源自天然。
+            </div>
+            <div class="read-more">
+              <span>查看更多</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="product-banner">
+        <div class="cover-model">
+          <!--产品文字说明-->
+          <div class="pro-msg">
+            <div class="pro-icon-area">
+              <div class="pro-icon pro-icon1"></div>
+            </div>
+            <div class="title">护肤品</div>
+            <div class="title">SKIN CARE</div>
+            <div class="detail">
+              汉萃主营产品是天然植物健康产品、芳香疗法和护肤肽类产品；研发6大系列40余种无添加任何化学成分的复方功效精油，已申请精油相关发明专利9项、肽相关发明专利7。每款产品针对不同肌肤问题提供专业解决方案。产品专注于安全健康，源自天然。
+            </div>
+            <div class="read-more">
+              <span>查看更多</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="page">
       <p>Page 3</p>
@@ -36,6 +87,8 @@
 </script>
 
 <style lang="scss">
+  @import "@/assets/styles/_mixins.scss";
+
   html, body, #app {
     overflow: hidden;
     height: 100%;
@@ -49,17 +102,81 @@
       justify-content: center;
       align-items: center;
     }
+    .page-first {
+      background-image: url("~@IMG/1.jpg");
+      @include background-cover-center();
+    }
+    .page-second {
+      background: #069163 no-repeat center/cover;
+      .product-banner {
+        width: 33.33%;
+        height: 100%;
+        background-image: url("~@IMG/pro-banner.png");
+        @include background-cover-center();
+        position: relative;
+        .cover-model {
+          @include position-ab();
+          background-color: rgba(6, 145, 99, 0.9);
+          z-index: 10;
+          &:hover {
+            background: none;
+            transition: all 1s linear;
+          }
+          &:hover > .pro-msg{
+            background-color: rgba(6, 145, 99, 0.9);
+          }
+        }
+        .pro-msg {
+          width: 276px;
+          height: 276px;
+          @include position-ab();
+          z-index: 20;
+          margin: auto;
+          border-radius: 50%;
+          .pro-icon-area {
+            display: flex;
+            justify-content: center;
+            margin-top: 27px;
+            margin-bottom: 19px;
+            .pro-icon {
+              width: 20px;
+              height: 21px;
+            }
+            .pro-icon1 {
+              background-image: url("~@IMG/Skin-care.png");
+              @include background-cover-center(20px, 21px);
+            }
+          }
+          .title {
+            font-size: 10px;
+            font-weight: 400;
+            color: #FEFEFE;
+            text-align: center;
+            padding-bottom: 4px;
+          }
+          .detail {
+            font-size: 9px;
+            color: #FEFEFE;
+            padding: 0 44px;
+            line-height: 1.2;
+          }
+          .read-more {
+            text-align: center;
+            span {
+              display: inline-block;
+              font-size: 10px;
+              color: #FEFEFE;
+              padding: 9px 0;
+              border-bottom: 1px solid #FFFFFF;
+              cursor: pointer;
+            }
+          }
+        }
+      }
+    }
 
     .page p {
       font-size: 66px;
-    }
-
-    .page:nth-child(1) {
-      background: lightpink no-repeat center/cover;
-    }
-
-    .page:nth-child(2) {
-      background: lightgreen no-repeat center/cover;
     }
 
     .page:nth-child(3) {
